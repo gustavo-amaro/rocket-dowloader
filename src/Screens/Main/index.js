1be4a9e50a6db6c0f107b4edde276da9c554abff
Gustavo <gustavo.amaro97@gmail.com>
2020-12-15 21:33:48 -0300

    feat: initial ui

--- src/Screens/Main/index.js
+++ src/Screens/Main/index.js
@@ -1,14 +1,22 @@
-import React, { Component } from "react"; // import from react
+import React, { useState } from "react"; // import from react
 
-import { Window, App } from "proton-native"; // import the proton-native components
+import { Window, App, TextInput, TouchableOpacity, Text } from "proton-native"; // import the proton-native components
 import styles from "./styles";
 
 export default function Main(){
+    const [downloadUrl, setDownloadUrl] = useState('');
+
+    function dowloadFile(){
+        console.log(downloadUrl);
+    }
     return (
       <App>
         {/* you must always include App around everything */}
         <Window style={styles.mainWindow}>
-          
+            <TextInput style={styles.urlInput} onChangeText={value => setDownloadUrl(value)}/>
+            <TouchableOpacity style={styles.buttonDownload} onPress={dowloadFile}>
+                <Text style={styles.textButton}>Download</Text>
+            </TouchableOpacity>
         </Window>
       </App>
     );
--- src/Screens/Main/styles.js
+++ src/Screens/Main/styles.js
@@ -3,6 +3,37 @@ import {StyleSheet} from 'proton-native';
 
 export default StyleSheet.create({
     mainWindow: {
-        width: 700, height: 500, backgroundColor: "#32353b" 
+        width: 700, 
+        height: 500, 
+        backgroundColor: "#32353b",
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column'
+    },
+    buttonDownload: {
+        height: 50,
+        minWidth: 120,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 50,
+        backgroundColor: '#8257e6',
+        borderRadius: 8
+    },
+    textButton: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#fff'
+    },
+    urlInput: {
+        padding: 6,
+        width: '50%',
+        borderRadius: 8,
+        borderColor: '#8257e6',
+        borderWidth: 1,
+        color: '#000',
+        fontWeight: 'bold',
+        fontSize: 12,
+        backgroundColor: '#fff',
     }
 })
